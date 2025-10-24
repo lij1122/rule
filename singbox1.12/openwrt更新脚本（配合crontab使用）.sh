e2cbff0e869cc317d00455bf3221d2b4d5994e33
lij1122 <122663073+lij1122@users.noreply.github.com>
2025-10-24 09:07:21 +0800

    Update openwrt更新脚本（配合crontab使用）.sh

--- singbox1.12/openwrt更新脚本（配合crontab使用）.sh
+++ singbox1.12/openwrt更新脚本（配合crontab使用）.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-echo "[$(date)] 开始更新 config.json" >> /etc/singbox/update.log
+echo "[$(date)] 开始更新 config.json" >> /etc/sing-box/update.log
 curl -s -o /etc/sing-box/config.json <http://你在sub-store中的模板链接>
 
 if [ $? -eq 0 ]; then
-  echo "[$(date)] 下载成功，重启 sing-box" >> /etc/singbox/update.log
+  echo "[$(date)] 下载成功，重启 sing-box" >> /etc/sing-box/update.log
   /etc/init.d/sing-box stop >/dev/null 2>&1
   /etc/init.d/sing-box start
 else
-  echo "[$(date)] ❌ 下载失败，请检查网络或链接" >> /etc/singbox/update.log
+  echo "[$(date)] ❌ 下载失败，请检查网络或链接" >> /etc/sing-box/update.log
 fi
